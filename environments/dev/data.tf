3a73568c4994b6056bf43edaf3ed74c63c69ed36
jiazhi110 <206487975@qq.com>
2025-11-20 17:20:58 +0800

    fix: update names of images.

--- environments/dev/data.tf
+++ environments/dev/data.tf
@@ -3,9 +3,9 @@ data "aws_availability_zones" "available" {
 }
 
 data "aws_ssm_parameter" "flink_image_url" {
-  name = "/data-platform/dev/ingestion/flink_image_url"
+  name = "/data-platform/dev/ingestion/flink_job_image_url"
 }
 
 data "aws_ssm_parameter" "mockdata_image_url" {
-  name = "/data-platform/dev/ingestion/mockdata_image_url"
+  name = "/data-platform/dev/ingestion/mockdata_producer_image_url"
 }
